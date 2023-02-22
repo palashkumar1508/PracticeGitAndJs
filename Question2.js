dec7fcf1e61414f3f3c083e3c78cf732d08732ed
palashkumar1508 <palashkumar1508@vidyamantra.com>
2023-02-22 10:57:54 +0530

    Find Leader solved

--- Question2.js
+++ Question2.js
@@ -18,14 +18,14 @@
 let n = 6 ;
 let A = [16,17,4,3,5,2];
 
-for(let i=0 ; i<n ; i++){
-    let count =0 ;
-    for(let j=i+1 ; j<n ; j++ ){
-        if(A[i] < A[j]){
+for(let i=0 ; i<n ; i++){   //itterating array element from array A upto length n
+    let count =0 ;          //taken a temp count var and initialize it 0
+    for(let j=i+1 ; j<n ; j++ ){// used for loop for  comparing i and j value from the where j is 1 greater than 1
+        if(A[i] < A[j]){        // if any value after A[i]  greater than then increase the count by 1
             count++;
         }
     }
-    if(count ==0){
+    if(count ==0){              // if the count is zero means no value is greater than in the right then print the value
         console.log(A[i]);
     }
 }
